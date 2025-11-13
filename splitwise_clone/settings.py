2ab438ed4040d98398f3fb206a7e59251756c023
batmansucksatcoding <121473858+batmansucksatcoding@users.noreply.github.com>
2025-11-13 13:13:43 +0530

    Update settings.py

--- splitwise_clone/settings.py
+++ splitwise_clone/settings.py
@@ -15,10 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'inster your key here'
 
@@ -82,9 +78,6 @@ WSGI_APPLICATION = 'splitwise_clone.wsgi.application'
 ASGI_APPLICATION = 'splitwise_clone.asgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -92,11 +85,8 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'accounts.User'  # custom user model
+AUTH_USER_MODEL = 'accounts.User'  
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -126,9 +116,6 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'your-google-client-secret'
 SOCIAL_AUTH_GITHUB_KEY = 'your-github-client-id'
 SOCIAL_AUTH_GITHUB_SECRET = 'your-github-client-secret'
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Kolkata'
@@ -137,14 +124,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -185,7 +165,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'index'
-    # after logout → back to landing
+
 
 # Email backend for development (prints reset link in console)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
