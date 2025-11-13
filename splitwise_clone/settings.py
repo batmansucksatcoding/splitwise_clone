3e9f9edda15d492ede8ddb06b7ee398fe9c3b59d
batmansucksatcoding <121473858+batmansucksatcoding@users.noreply.github.com>
2025-11-13 13:12:33 +0530

    Update SECRET_KEY placeholder in settings.py
    Replaced the secret key placeholder for production.

--- splitwise_clone/settings.py
+++ splitwise_clone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kh-lq+ir669&hdywmhxudiu6f5x!33xcg+81vu@c3s_3_q@id8'
+SECRET_KEY = 'inster your key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
